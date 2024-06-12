9e8ebf8fccd40c887adaae5b39069e532c0732c7
Tejeshwar-SM <work10tejeshwar@gmail.com>
2024-06-12 23:04:34 +0530

    exrcise05-12Jun

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if(num1<0 || num2<0)  {
+        return 'ERROR';
+    }
+    // if(typeof num1!=='number' || typeof num2 !== 'number') {
+    //     return 'ERROR';
+    // }
 
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    }
+    let num = Math.min(num1,num2); 
+    while(num<=Math.max(num1,num2)) {
+        sum += num;
+        num++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
